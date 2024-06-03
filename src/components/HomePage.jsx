d8a0c86b00787ca06c1eaeafcb77303c85e55fac
nguyenbalinh1401 <linhnbse161930@fpt.edu.vn>
2024-06-03 21:44:18 +0700

    linh

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,122 +1,144 @@
-import React from 'react';
-import { LaptopOutlined, NotificationOutlined, UserOutlined } from '@ant-design/icons';
-import { Breadcrumb, Layout, Menu, theme } from 'antd';
-import { Link } from 'react-router-dom';
-
+import React from "react";
+import {
+  LaptopOutlined,
+  NotificationOutlined,
+  UserOutlined,
+} from "@ant-design/icons";
+import { Breadcrumb, Layout, Menu, theme } from "antd";
+import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+import axios from "axios";
 const { Header, Content, Footer, Sider } = Layout;
 
-const items1 = ['Home', 'Buy', 'Sell'].map((label, index) => ({
+const items1 = ["Home", "Buy", "Sell"].map((label, index) => ({
   key: String(index + 1),
   label,
 }));
 
-const items2 = [UserOutlined, LaptopOutlined, NotificationOutlined].map((icon, index) => {
-  const key = String(index + 1);
-  return {
-    key: `sub${key}`,
-    icon: React.createElement(icon),
-    label: `subnav ${key}`,
-    children: new Array(4).fill(null).map((_, j) => {
-      const subKey = index * 4 + j + 1;
-      return {
-        key: subKey,
-        label: `option${subKey}`,
-      };
-    }),
-  };
-});
+const items2 = [UserOutlined, LaptopOutlined, NotificationOutlined].map(
+  (icon, index) => {
+    const key = String(index + 1);
+    return {
+      key: `sub${key}`,
+      icon: React.createElement(icon),
+      label: `subnav ${key}`,
+      children: new Array(4).fill(null).map((_, j) => {
+        const subKey = index * 4 + j + 1;
+        return {
+          key: subKey,
+          label: `option${subKey}`,
+        };
+      }),
+    };
+  }
+);
 
-export default function HomePage ()  {
+export default function HomePage() {
   const {
     token: { colorBgContainer, borderRadiusLG },
   } = theme.useToken();
 
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    const getData = () => {
+      axios
+        .get("http://localhost:3000/product")
+        .then((res) => {
+          console.log(res.data);
+          setData(res.data);
+        })
+        .catch((err) => console.log(err));
+    };
+    getData();
+  }, []);
   return (
-    <Layout style={{ minHeight: '100vh' }}>
-      <Header
-        style={{
-          display: 'flex',
-          alignItems: 'center',
-        }}
-      >
-        <div className="demo-logo" />
-        <Menu
-          theme="dark"
-          mode="horizontal"
-          defaultSelectedKeys={['1']}
-          items={items1}
+    <>
+      <Layout style={{ minHeight: "100vh" }}>
+        <Header
           style={{
+            display: "flex",
+            alignItems: "center",
+          }}
+        >
+          <div className="demo-logo" />
+          <Menu
+            theme="dark"
+            mode="horizontal"
+            defaultSelectedKeys={["1"]}
+            items={items1}
+            style={{
+              flex: 1,
+              minWidth: 0,
+            }}
+          />
+        </Header>
+        <Content
+          style={{
+            padding: "0 48px",
             flex: 1,
-            minWidth: 0,
-          }}
-        />
-      </Header>
-      <Content
-        style={{
-          padding: '0 48px',
-          flex: 1,
-        }}
-      >
-        <Breadcrumb
-          style={{
-            margin: '16px 0',
           }}
         >
-          <Breadcrumb.Item>
-            <Link to="/">Home</Link>
-          </Breadcrumb.Item>
-          <Breadcrumb.Item>
-            <Link to="/list">List</Link>
-          </Breadcrumb.Item>
-          <Breadcrumb.Item>
-            <Link to="/app">App</Link>
-          </Breadcrumb.Item>
-        </Breadcrumb>
-        <Layout
-          style={{
-            padding: '24px 0',
-            background: colorBgContainer,
-            borderRadius: borderRadiusLG,
-          }}
-        >
-          <Sider
+          <Breadcrumb
             style={{
+              margin: "16px 0",
+            }}
+          >
+            <Breadcrumb.Item>
+              <Link to="/">Home</Link>
+            </Breadcrumb.Item>
+            <Breadcrumb.Item>
+              <Link to="/list">List</Link>
+            </Breadcrumb.Item>
+            <Breadcrumb.Item>
+              <Link to="/app">App</Link>
+            </Breadcrumb.Item>
+          </Breadcrumb>
+          <Layout
+            style={{
+              padding: "24px 0",
               background: colorBgContainer,
+              borderRadius: borderRadiusLG,
             }}
-            width={200}
           >
-            <Menu
-              mode="inline"
-              defaultSelectedKeys={['1']}
-              defaultOpenKeys={['sub1']}
+            <Sider
               style={{
-                height: '100%',
+                background: colorBgContainer,
               }}
-              items={items2}
-            />
-          </Sider>
-          <Content
-            style={{
-              padding: '0 24px',
-              minHeight: 280,
-            }}
-          >
-            Content
-          </Content>
-        </Layout>
-      </Content>
-      <Footer
-        style={{
-          backgroundColor: '#282c34',
-          padding: '10px',
-          color: 'white',
-          textAlign: 'center',
-        }}
-      >
-        Linh Design ©{new Date().getFullYear()} Created by Linh
-      </Footer>
-    </Layout>
+              width={200}
+            >
+              <Menu
+                mode="inline"
+                defaultSelectedKeys={["1"]}
+                defaultOpenKeys={["sub1"]}
+                style={{
+                  height: "100%",
+                }}
+                items={items2}
+              />
+            </Sider>
+            <Content
+              style={{
+                padding: "0 24px",
+                minHeight: 280,
+              }}
+            >
+              {data.map((item) => {
+                return <div>{item.name}</div>;
+              })}
+            </Content>
+          </Layout>
+        </Content>
+        <Footer
+          style={{
+            backgroundColor: "#282c34",
+            padding: "10px",
+            color: "white",
+            textAlign: "center",
+          }}
+        >
+          Linh Design ©{new Date().getFullYear()} Created by Linh
+        </Footer>
+      </Layout>
+    </>
   );
-};
-
-
+}
