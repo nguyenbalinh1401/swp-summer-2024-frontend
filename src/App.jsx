5ed10bbb86fd86cbd00f6f841d8fdaa081ce9d3f
ltrungg <trungagchw@gmail.com>
2024-06-15 15:40:53 +0700

    sap done product detail

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,12 @@ import Navbar from "./components/navbar/Navbar";
 import Footer from "./components/footer/Footer";
 import AppRouter from "./components/appRouter/AppRouter";
 import { CookiesProvider } from "react-cookie";
-import "./App.css";
-import "./index.css";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import HomePage from "./components/HomePage";
-import ListPage from "./components/ListPage";
-import AppPage from "./components/AppPage";
+// import "./App.css";
+// import "./index.css";
+// import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+// import HomePage from "./components/HomePage";
+// import ListPage from "./components/ListPage";
+// import AppPage from "./components/AppPage";
 
 function App() {
   return (
