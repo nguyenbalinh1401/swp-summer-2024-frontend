aa0c46973c0d559094dbdbbf2ce44c6b2a4a63af
nguyenbalinh1401 <linhnbse161930@fpt.edu.vn>
2024-06-15 16:29:46 +0700

    Linh

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -126,7 +126,7 @@ export default function Home() {
           <Row gutter={[8,8]}>
             {products.map((product) => (
               <Col key={product.id} span={6}>
-                <Link to={'/HomePage/${product.id}'}>
+                <Link to={`/product/${product.id}`}>
                   <Card
                     hoverable
                     cover={<img alt={product.name} src={product.image} />}
